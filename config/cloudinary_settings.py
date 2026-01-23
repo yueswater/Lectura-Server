4138437bb38dae7316e3fff72d15e8ecdfad65dd
Anthony Sung <sungpinyue@gmail.com>
2026-01-23 19:32:49 +0800

    fix: image failed to load in exported PDF file

--- config/cloudinary_settings.py
+++ config/cloudinary_settings.py
@@ -1,9 +1,9 @@
 import os
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
